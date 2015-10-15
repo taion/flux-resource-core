6462c3c5311ae558c4e3fd35be062492f889658a
Jimmy Jia <tesrin@gmail.com>
2015-10-14 23:04:59 -0400

    Let subclasses hook into API execution

--- src/api.js
+++ src/api.js
@@ -19,57 +19,57 @@ export default function generateApi(
 ) {
   const {fetch, methodNames} = this;
 
-  function doFetch(method, {id, data, options}) {
-    const urlBase = urlFunc(id || '');
-    const queryString = this.formatQueryString(options);
-    const url = queryString ? `${urlBase}?${queryString}` : urlBase;
-
-    const processorOptions = {id, ...options};
-
-    let request = {method, body: data};
-    preprocessors.forEach(preprocessor => {
-      const newRequest = this::preprocessor(request, processorOptions);
-      if (newRequest !== undefined) {
-        request = newRequest;
-      }
-    });
-
-    let result = this::fetch(url, request);
-    postprocessors.forEach(postprocessor => {
-      result = result.then(response => {
-        const newResponse = this::postprocessor(response, processorOptions);
-        return newResponse !== undefined ? newResponse : response;
-      });
-    });
-
-    return result;
-  }
-
   return {
     formatQueryString,
 
+    execute(method, {url, id, data, options}) {
+      const urlBase = url || urlFunc(id || '');
+      const queryString = this.formatQueryString(options);
+      const urlFull = queryString ? `${urlBase}?${queryString}` : urlBase;
+
+      const processorOptions = {id, ...options};
+
+      let request = {method, body: data};
+      preprocessors.forEach(preprocessor => {
+        const newRequest = this::preprocessor(request, processorOptions);
+        if (newRequest !== undefined) {
+          request = newRequest;
+        }
+      });
+
+      let result = this::fetch(urlFull, request);
+      postprocessors.forEach(postprocessor => {
+        result = result.then(response => {
+          const newResponse = this::postprocessor(response, processorOptions);
+          return newResponse !== undefined ? newResponse : response;
+        });
+      });
+
+      return result;
+    },
+
     [methodNames.getMany](options) {
-      return this::doFetch('GET', {options});
+      return this.execute('GET', {options});
     },
 
     [methodNames.getSingle](id, options) {
-      return this::doFetch('GET', {id, options});
+      return this.execute('GET', {id, options});
     },
 
     [methodNames.postSingle](data, options) {
-      return this::doFetch('POST', {data, options});
+      return this.execute('POST', {data, options});
     },
 
     [methodNames.putSingle](id, data, options) {
-      return this::doFetch('PUT', {id, data, options});
+      return this.execute('PUT', {id, data, options});
     },
 
     [methodNames.patchSingle](id, data, options) {
-      return this::doFetch('PATCH', {id, data, options});
+      return this.execute('PATCH', {id, data, options});
     },
 
     [methodNames.deleteSingle](id, options) {
-      return this::doFetch('DELETE', {id, options});
+      return this.execute('DELETE', {id, options});
     }
   };
 }
